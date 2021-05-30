df390213da838e5724338c4b4de39c88df1d1c50
Somsubhra Das <das.somsubhra1@gmail.com>
2021-05-30 12:00:28 +0530

    model and serializer created for lead

---
+++ leads/serializers.py
@@ -0,0 +1,11 @@
+
+from rest_framework import serializers
+from .models import Lead
+
+# Lead serializer
+
+
+class LeadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lead,
+        fields = '__all__'
