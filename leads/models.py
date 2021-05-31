744e464597095cd794f102443f4fc19aca262b69
Somsubhra Das <das.somsubhra1@gmail.com>
2021-05-31 12:10:56 +0530

    Leads model updated

--- leads/models.py
+++ leads/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,6 +9,7 @@ class Lead(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, unique=True)
     message = models.CharField(max_length=500, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     # def __str__(self):
