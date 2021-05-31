3e125c90745aabd53e9ee69231903f979f1b4997
Somsubhra Das <das.somsubhra1@gmail.com>
2021-05-31 12:24:59 +0530

    added permissions and changed queryset in leads viewset

--- leads/api.py
+++ leads/api.py
@@ -6,8 +6,17 @@ from .serializers import LeadSerializer
 # Lead ViewSet
 # A view set gives crud based api service without explicitly writing routes for all methods
 class LeadViewSet(viewsets.ModelViewSet):
-    queryset = Lead.objects.all()
+    # queryset = Lead.objects.all()
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticated
     ]
+
     serializer_class = LeadSerializer
+
+    # Changing queryset to display just the leads of authenticated user
+    def get_queryset(self):
+        return super().request.user.leads.all()
+
+    # Adding owner property to request body data
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
